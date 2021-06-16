4106831e2afebe603ede710d68b14be1c0e84371
MEG HOWE <howem9@gmail.com>
2021-06-16 14:29:31 -0400

    Added image address urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-product = Product.create(name: "Aquarium_tank", price: 1000000, image_url: "", description: "Really big and made of mermaid glass")
+product = Product.create(name: "Aquarium_tank", price: 1000000, image_url: "http://sc04.alicdn.com/kf/HTB1CZTxX.T1gK0jSZFhq6yAtVXaH.jpg", description: "Really big and made of mermaid glass")
 
-product = Product.create(name: "Shark", price: 20, image_url: "", description: "Buy 2 sharks, get one free")
+product = Product.create(name: "Shark", price: 20, image_url: "https://frontiersinblog.files.wordpress.com/2020/01/frontiers-marine-science-estimating-global-catches-marine-recreational-fishes.jpg?w=1000", description: "Buy 2 sharks, get one free")
 
-product = Product.create(name: "Nemo", price: 50, image_url: "", description: "Have you seen my dad?")
+product = Product.create(name: "Nemo", price: 50, image_url: "https://upload.wikimedia.org/wikipedia/commons/9/9b/A._percula.jpg", description: "Have you seen my dad?")
 
-product = Product.create(name: "Starfish", price: 100, image_url: "", description: "I did a report on starfish in 1st grade. Their arms grow back!")
+product = Product.create(name: "Starfish", price: 100, image_url: "https://www.hdwallpapersnature.com/wp-content/uploads/2020/08/Pink-Starfish-Hd-Wallpaper-9.jpg", description: "I did a report on starfish in 1st grade. Their arms grow back!")
