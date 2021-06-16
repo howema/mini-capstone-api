e2fe8cf792a39e8c394b2c35886a3ca3da8ff168
MEG HOWE <howem9@gmail.com>
2021-06-16 14:03:10 -0400

    created database, migrated, made product instances, seed thing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product = Product.create(name: "Aquarium_tank", price: 1000000, image_url: "", description: "Really big and made of mermaid glass")
+
+product = Product.create(name: "Shark", price: 20, image_url: "", description: "Buy 2 sharks, get one free")
+
+product = Product.create(name: "Nemo", price: 50, image_url: "", description: "Have you seen my dad?")
+
+product = Product.create(name: "Starfish", price: 100, image_url: "", description: "I did a report on starfish in 1st grade. Their arms grow back!")
