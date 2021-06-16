71006215fd5694b0ca6ab04be3291bbc16aa0113
MEG HOWE <howem9@gmail.com>
2021-06-16 14:14:47 -0400

    Added routes and controllers for all individual products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,24 @@ class ProductsController < ApplicationController
     product = Product.all
     render json: product.as_json
   end
+
+  def Aquarium_tank
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def Shark
+    product = Product.second
+    render json: product.as_json
+  end
+
+  def Nemo
+    product = Product.third
+    render json: product.as_json
+  end
+
+  def Starfish
+    product = Product.fourth
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/all_products", controller: "products", action: "all_products"
+
+  get "/aquarium_path", controller: "products", action: "Aquarium_tank"
+
+  get "/shark_path", controller: "products", action: "Shark"
+
+  get "/nemo_path", controller: "products", action: "Nemo"
+
+  get "/starfish_path", controller: "products", action: "Starfish"
 end
