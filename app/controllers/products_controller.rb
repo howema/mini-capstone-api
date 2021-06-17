d80b16268a855e9bedb0df9309dcde41d14fd0b7
MEG HOWE <howem9@gmail.com>
2021-06-17 15:13:59 -0400

    Added 2 types of parameters

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,38 @@
 class ProductsController < ApplicationController
-  def all_products
-    product = Product.all
-    render json: product.as_json
+  # def all_products
+  #   product = Product.all
+  #   render json: product.as_json
+  # end
+
+  # def Aquarium_tank
+  #   product = Product.first
+  #   render json: product.as_json
+  # end
+
+  # def Shark
+  #   product = Product.second
+  #   render json: product.as_json
+  # end
+
+  # def Nemo
+  #   product = Product.third
+  #   render json: product.as_json
+  # end
+
+  # def Starfish
+  #   product = Product.fourth
+  #   render json: product.as_json
+  # end
+
+  def shark
+    input = params["key"]
+    render json: { message: "The url segment is #{input}" }
   end
 
-  def Aquarium_tank
-    product = Product.first
-    render json: product.as_json
-  end
-
-  def Shark
-    product = Product.second
-    render json: product.as_json
-  end
-
-  def Nemo
-    product = Product.third
-    render json: product.as_json
-  end
-
-  def Starfish
-    product = Product.fourth
-    render json: product.as_json
+  def aquarium_tank
+    input = params["query"]
+    output_message = input
+    output_message = "Welcome to Best Aquarium Buy! So, you want a tank?"
+    render json: { message: output_message }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/all_products", controller: "products", action: "all_products"
+  # get "/all_products", controller: "products", action: "all_products"
 
-  get "/aquarium_path", controller: "products", action: "Aquarium_tank"
+  # get "/aquarium_path", controller: "products", action: "Aquarium_tank"
 
-  get "/shark_path", controller: "products", action: "Shark"
+  # get "/shark_path", controller: "products", action: "Shark"
 
-  get "/nemo_path", controller: "products", action: "Nemo"
+  # get "/nemo_path", controller: "products", action: "Nemo"
 
-  get "/starfish_path", controller: "products", action: "Starfish"
+  # get "/starfish_path", controller: "products", action: "Starfish"
+
+  # get "/shark_path" => "products#starfish"
+
+  get "/shark_path/:key" => "products#shark" #url segment parameter
+
+  get "/aquarium_path" => "products#aquarium_tank" #query parameters
 end
