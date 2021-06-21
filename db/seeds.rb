91c4754412e34c50b6cb90edbc50874795c9562c
MEG HOWE <howem9@gmail.com>
2021-06-21 15:56:58 -0400

    Added new products, removed all blanks and duplicates

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# product.destroy_all
+Product.destroy_all
 
 product = Product.create(name: "Aquarium_tank", price: 1000000, image_url: "http://sc04.alicdn.com/kf/HTB1CZTxX.T1gK0jSZFhq6yAtVXaH.jpg", description: "Really big and made of mermaid glass")
 
@@ -14,4 +14,12 @@ product = Product.create(name: "Shark", price: 20, image_url: "https://frontiers
 
 product = Product.create(name: "Nemo", price: 50, image_url: "https://upload.wikimedia.org/wikipedia/commons/9/9b/A._percula.jpg", description: "Have you seen my dad?")
 
-product = Product.create(name: "Starfish", price: 100, image_url: "https://www.hdwallpapersnature.com/wp-content/uploads/2020/08/Pink-Starfish-Hd-Wallpaper-9.jpg", description: "I did a report on starfish in 1st grade. Their arms grow back!")
+product = Product.create(name: "Starfish", price: 100, image_url: "https://www.hdwallpapersnature.com/wp-content/uploads/2020/08/Pink-Starfish-Hd-Wallpaper-9.jpg", description: "Their arms grow back!")
+
+product = Product.create(name: "Big Fish", price: 5, image_url: "https://reelclub.files.wordpress.com/2012/05/bf1.jpg", description: "It's a play on words.")
+
+product = Product.create(name: "Leviathan", price: 500000, image_url: "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/8dd71862-4114-4978-9913-b390e641e461/ddpd24l-85ac46ad-25a4-4863-8fca-20408babdbaa.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzhkZDcxODYyLTQxMTQtNDk3OC05OTEzLWIzOTBlNjQxZTQ2MVwvZGRwZDI0bC04NWFjNDZhZC0yNWE0LTQ4NjMtOGZjYS0yMDQwOGJhYmRiYWEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.yU4U7WGat-6Qg3UHi20XjAiPOvealRnyGvaHEc06LzI", description: "Reaper class. Highly agressive.")
+
+product = Product.create(name: "Stingray", price: 30, image_url: "https://images.unsplash.com/photo-1509414016757-23d57a425e06?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c3RpbmdyYXl8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80", description: "Colorful sea pancake.")
+
+product = Product.create(name: "Colorful coral", price: 18, image_url: "https://media.fromthegrapevine.com/assets/images/2015/6/Colorful%20coral%20reef.jpg.824x0_q71_crop-scale.jpg", description: "Much more popular than our greyscale coral.")
