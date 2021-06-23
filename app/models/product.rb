729b9965dba6eafc1e2754abbcadf3aec78e5fa4
MEG HOWE <howem9@gmail.com>
2021-06-23 13:00:44 -0400

    Added validations and new products. Note- Add Description on frontend does not work

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :description, presence: true
+  validates :name, uniqueness: true
+  validates :price, numericality: true
+
   def is_discounted?
     if price < 10
       return true
